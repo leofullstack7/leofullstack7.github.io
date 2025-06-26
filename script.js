00864dc6c9f8db394a6d542782bed448c13cc0c2
leofullstack7 <lgarcessa@gmail.com>
2025-06-26 08:09:32 -0500

    foto

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function downloadPDF(){
-    console.log("El botón fue presionado.");
+function downloadPDF() {
     const element = document.querySelector('.container-lg');
-    const opt = {
-        margin: 0,
-        filename:'Hoja_de_Vida_Leandro_Garces.pdf',
-        image: {type: 'jpg', quality:1},
-        html2canvas: {scale:2, useCORS:true},
-        jsPDF: {unit:'in', format:'letter', orientation:'portrait'}
-    };
-    html2pdf().set(opt).from(element).save()
-        .then(() => {
-            console.log("PDF generado exitosamente.");
-        })
-        .catch(err => {
-            console.error("Error al generar el PDF:", err);
-        });
 
+    const opt = {
+        margin: [10, 5, 55 , 5],  // [arriba, izquierda, abajo, derecha] en mm
+        filename: 'Hoja_de_Vida_Leandro_Garces.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: {
+            scale: 2,
+            useCORS: true,
+            allowTaint: false,
+            scrollY: 0
+        },
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        }
+    };
+
+    html2pdf().set(opt).from(element).save();
 }
